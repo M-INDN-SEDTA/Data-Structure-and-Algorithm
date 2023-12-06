909b7457b67b61e0edb22afd730703f419e73b12
M-INDN-SEDTA <115917863+M-INDN-SEDTA@users.noreply.github.com>
2023-12-06 11:03:08 +0530

    final commit.

--- Pattern_butterfly.cpp
+++ Pattern_butterfly.cpp
@@ -9,7 +9,7 @@ int main()
    for(int i=1;i<=n;i++)
    {
     for(int j=1;j<=i;j++)
-    { cout<<"*";      }
+    { cout<<"*";      } // Replace * with other symbol.
     int space= 2*n - 2*i ;
     for(int j=1;j<=space;j++)
     {   cout<<" ";}
