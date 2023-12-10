4fcab0ab92d942fcbe95069df9befc358405d59a
M-INDN-SEDTA <115917863+M-INDN-SEDTA@users.noreply.github.com>
2023-12-10 05:57:30 +0530

    initial commit

---
+++ Pattern_half_pyramid_no.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+  cout<<"Enter no. :"<<endl;
+  int n;
+  cin>>n;
+
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=n;j++)
+    {
+        if (j<=i)
+        {
+            cout<<i;
+        }
+        else
+        cout<<" ";
+    }
+   cout<<endl;
+  }
+}
